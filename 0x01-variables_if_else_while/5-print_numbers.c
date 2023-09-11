71aaf34988dcd6ae7099d31f53ac2711b047667e
Janeucode <enegbedeochuko@gmail.com>
2023-09-11 15:59:21 -0700

    initial try

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++0)
+	putchar(i);
+	putchar('\n');
+	return(0);
+}
+

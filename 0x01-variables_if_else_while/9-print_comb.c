062770c3f98d8349ca20957bd755e7bd1490efef
Janeucode <enegbedeochuko@gmail.com>
2023-09-11 19:29:30 -0700

    love

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,10 +10,13 @@ int main(void)
 	int i;
 
 	for (i = '0'; i <= '9'; i++)
+	{
 		putchar(i);
-	if (i < '9') {
-		putchar(',');
-		putchar(' ');
+		if (i < '9') 
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);

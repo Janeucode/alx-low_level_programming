9f5311e865aa2a5e00b1b0bfdf5d4958d734da19
Janeucode <enegbedeochuko@gmail.com>
2023-09-11 18:26:51 -0700

    write multiple digits without error

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,9 +9,10 @@ int main(void)
 {
 	char i;
 
-	for (i = '0'; i < '100'; i++)
+	for (i = '0'; i < 'd'; i++)
 		putchar(i);
-	if (i < 99)
+	if (i < 'c')
 		putchar(', ');
+	putchar('\n')
 	return (0);
 }

b568d7edb9164d585559fa7e3acc97f5401833d3
Janeucode <enegbedeochuko@gmail.com>
2023-09-11 17:20:52 -0700

    try hexa spec on char

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,14 +2,14 @@
 /**
  * main - Entry point
  *
- * Return - always 0 (success)
+ * Return: always 0 (success)
  */
 int main(void)
 {
 	char i;
 
 	for (i = '0'; i <= 'f'; i++)
-	putchar (i);
+	putchar (iX);
 	putchar ('\n');
 	return (0);
 }

7ba09271b41400f018576d09d838c63d1fd18642
Janeucode <enegbedeochuko@gmail.com>
2023-09-12 20:04:45 -0700

    nawa

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,8 +7,24 @@
  *
  * Return: always 0 (success)
  */
+void print_alphabet(void)
+{
+    int i;
+    char message[2]; // Buffer to hold the character and null terminator
+
+    for (i = 'a'; i <= 'z'; i++)
+    {
+        message[0] = (char)i; // Convert int to char
+        message[1] = '\0';    // Null-terminate the string
+
+        write(1, message, strlen(message));
+    }
+
+    putchar('\n'); // Use putchar to print a newline character
+}
+
 int main(void)
 {
-	print_alphabet();
-	return 0;
+    print_alphabet();
+    return 0;
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,17 +4,9 @@ int _putchar(void)
 	write(1, message, strlen(message));
         return (0);
 }
-void print_alphabet(void)
-{
-	int i;
-	char message[2];
+#ifndef MAIN_H
+#define MAIN_H
 
-	for (i = 'a'; i <= 'z'; i++)
-	{
-		message[0] = (char)i;
-		message[1] = '\0';
-        	write(1, message, strlen(message));
-	}
-	putchar('\n');
-	return (void);
-}
+void print_alphabet(void);
+
+#endif /* MAIN_H */

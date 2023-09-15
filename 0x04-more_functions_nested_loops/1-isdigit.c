92bf9f7a8b798cc16d9314b05fa8777f399c5b09
Janeucode <enegbedeochuko@gmail.com>
2023-09-14 19:12:35 -0700

    k

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,9 +1,11 @@
 #include "main.h"
 /**_isdigit - checks for digits
  * @c: hold number
- * 
+ * Description: for checking if numbers are present
  * Return: 1 is number is present otherwise
  */
+
+
 int _isdigit(int c)
 {
 	if (c >=48 && c <=57)

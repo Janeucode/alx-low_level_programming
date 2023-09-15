b059a2271166bf011237e0e440d400da850c1028
Janeucode <enegbedeochuko@gmail.com>
2023-09-15 10:23:21 -0700

    thankgod

---
+++ 0x04-more_functions_nested_loops/3-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: print number from 0 - 9
+ *
+ * Return - always 0 (success)
+ */
+int main(void)
+{
+	print_numbers();
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - Entry point
+ * @i: holds numbers to be printed
+ * Description: print numbers ranging from 0 - 9
+ * Return - 0 if sucessful
+ */
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}

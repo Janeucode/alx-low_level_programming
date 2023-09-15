0de6c61acb5d71c9a96ab9bde37b55c553bd4510
Janeucode <enegbedeochuko@gmail.com>
2023-09-14 17:29:27 -0700

    abeg

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -32,8 +32,8 @@ int main(void)
 	char i;
 
 	i = 'A';
-	printf("%i: %d\n", i, _isupper(i));
+	printf("'%c': %d\n", i, _isupper(i));
 	i = 'a';
-	printf("%i: %d\n", i, _isupper(i));
+	printf("'%c': %d\n", i, _isupper(i));
 	return (0);
 }

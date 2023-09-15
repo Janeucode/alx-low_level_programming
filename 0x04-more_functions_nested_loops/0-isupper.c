9135ed58c39b57fc2b31c9b376423eaf6b1e3222
Janeucode <enegbedeochuko@gmail.com>
2023-09-14 19:00:27 -0700

    gg

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,9 @@
 #include "main.h"
-#include <unistd.h>
 /**
  * _isupper - this function checks if letters are in uper case
  * @i: the character to be checked (ASCII value)
  *
- * Return: 1 if @i is uppercase, 0 if @i is lower case and 2 if it is neither.
+ * Return: 1 if @i is uppercase, 0 if @i is lower case else 2 if it is neither.
  */
 int _isupper(int i)
 {
@@ -12,13 +11,5 @@ int _isupper(int i)
 	{
 		return (1);
 	}
-
-	else if (i >= 'a' && i <= 'z')
-	{
-		return (0);
-	}
-	else
-	{
-		return (2);
-	}
+	return (0);
 }

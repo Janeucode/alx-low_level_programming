5c9aad7c4c3c9f9ee8e1e784d805e674c386722d
Janeucode <enegbedeochuko@gmail.com>
2023-09-15 12:55:47 -0700

    gh

---
+++ 0x04-more_functions_nested_loops/5-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: print number from 0 - 9
+ *
+ * Return - always 0 (success)
+ */
+int main(void)
+{
+	most_numbers();
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * more_numbers - Entry point
+ * Description: print numbers times 10
+ * Return: always 0 (success)
+ */
+void more_numbers(void)
+{
+	char i, j;
+
+	for (j = '0'; j <= '14'; j++)
+	{
+		for (i = '0'; i <= '14'; i++)
+		{
+			if (i > 9)
+			{
+				continue;
+			}
+			_putchar(i + '0');
+		}
+	_putchar('\n');
+	}
+}

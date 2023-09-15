9e0f27f8a8b7d95f3abd67b94b4aa12d0d59af15
Janeucode <enegbedeochuko@gmail.com>
2023-09-15 14:56:49 -0700

    jk

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,15 +8,11 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (j = 0; j <= 14; j++)
+	for (j = 0; j < 10; j++)
 	{
-		for (i = 0; i <= 14; i++)
+		for (i = 48; i <= 62; i++)
 		{
-			if (i > 9)
-			{
-				continue;
-			}
-			_putchar(i + '0');
+			_putchar(i);
 		}
 	_putchar('\n');
 	}

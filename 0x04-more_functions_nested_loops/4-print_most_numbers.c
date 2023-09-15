ef7e8fbf8b5229926d93fbef2c7b43d6d27fb353
Janeucode <enegbedeochuko@gmail.com>
2023-09-15 11:10:40 -0700

    vex

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - Entry point
+ * Description: print numbers ranging from 0 - 9
+ * Return - 0 if sucessful
+ */
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i == '2' || i == '4')
+			continue;
+		_putchar(i);
+	}
+	_putchar('\n');
+}

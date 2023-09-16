a23310df38fe692f8981e2e61cf80fab6abb49fc
Janeucode <enegbedeochuko@gmail.com>
2023-09-15 17:46:04 -0700

    kls

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,19 +1,21 @@
 #include "main.h"
 /**
  * print_line - Entry point
+ * @n: takes input for number of lines to print
  * Description: print a straignt line
  * Return: always 0 (success)
  */
 void print_line(int n)
 {
 	int i;
+
 	if (n <= 0)
 	{
-		 _putchar('\n');
+		_putchar('\n');
 	}
 	else
 	{
-		for(i = 0; i < n; i++)
+		for (i = 0; i < n; i++)
 		{
 			_putchar('_');
 		}

c7491b04f9dfffab277fb8918cb2c0eb696d56f9
Janeucode <enegbedeochuko@gmail.com>
2023-09-15 19:41:03 -0700

    sjdfdfk

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main (void)
+{
+	int i;
+
+	i = o;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * positive_or_negative - Entry point
+ *
+ * Return: 0
+ */
+
+int positive_or_negative(char c)
+{
+	return (write(1, &c, 1));
+}

5383b9df9b919000c9bfc4ec62c27d14060d61ff
Janeucode <enegbedeochuko@gmail.com>
2023-09-15 17:38:22 -0700

    lebe

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,14 +7,16 @@
 void print_line(int n)
 {
 	int i;
-
-	for (i = 0; i < n; i++)
+	if (n <= 0)
 	{
-		_putchar('_');
+		 _putchar('\n');
 	}
-	if((n = 0) || n < 0)
+	else
 	{
+		for(i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }

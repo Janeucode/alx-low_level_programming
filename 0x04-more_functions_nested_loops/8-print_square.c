080cdc99c26fc12dcf57e7161e64fd5bed6cc36a
Janeucode <enegbedeochuko@gmail.com>
2023-09-15 19:19:16 -0700

    jksd

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_square - Entry point
+ * @size: takes input for number of squares to print
+ * Description: print a square
+ * Return: always 0 (success)
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0 ; j <= size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}

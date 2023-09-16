0a48d6b4def4b518bebd72d204351761f93ad396
Janeucode <enegbedeochuko@gmail.com>
2023-09-15 21:57:43 -0700

    dcsdcsdc

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+/**
+ * print_number - Entry point
+ * @n: number
+ * Return: 0
+ */
+
+void print_numbe(int n)
+{
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		i = -i;
+	}
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	_putchar((i % 10) + '0');
+}
--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,11 +8,15 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (j = 0; j < 10; j++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (i = 0; i <= 14; i++)
+		for (j = 0; j <= 14; j++)
 		{
-			_putchar(i + '0');
+			if (j >= 10)
+			{
+				_putchar((j / 10) + '0');
+			}
+			_putchar((j % 10) + '0');
 		}
 	_putchar('\n');
 	}

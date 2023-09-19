c5507aca0a0b08e1aa88989ca3620c6c1d072501
Janeucode <enegbedeochuko@gmail.com>
2023-09-18 17:56:04 -0700

    hala

---
+++ 0x05-pointers_arrays_strings/5-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char s[10] = "My School";
+
+	_putchar(s);
+	rev_string(s);
+	_putchar(s);
+	return (0);
+}

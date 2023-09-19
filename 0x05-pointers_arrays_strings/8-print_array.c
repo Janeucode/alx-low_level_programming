a350c2d7cff8abd5f71af19d22ee769f3f5721a5
Janeucode <enegbedeochuko@gmail.com>
2023-09-18 20:03:50 -0700

    sd

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,6 +3,7 @@
 /**
  * print_array - Entry point
  * @n: number of elements
+ * @a: second member
  * Return: void
  */
 void print_array(int *a, int n)
@@ -10,7 +11,9 @@ void print_array(int *a, int n)
 	int i;
 
 	if (n <= 0)
+	{
 		printf("\n");
+	}
 	for (i = 0; i < n; i++)
 	{	
 		printf("%d", a[i]);

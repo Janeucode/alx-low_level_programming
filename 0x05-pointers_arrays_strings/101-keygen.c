a89a22504b340539c36f6364171f097fc0094d62
Janeucode <enegbedeochuko@gmail.com>
2023-09-19 19:57:01 -0700

    sds

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#define PASSWORD_LENGTH 10
 /**
  * main -Entry point
  *
@@ -8,8 +9,11 @@
  */
 int main(void)
 {
+	int i;
+
+
 	srand((unsigned int)time(NULL));
-	const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	#define CHARSET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 	int charset_size = sizeof(charset) - 1;
 	printf("Generated Password: ");
 	for (i = 0; i < PASSWORD_LENGTH; i++)

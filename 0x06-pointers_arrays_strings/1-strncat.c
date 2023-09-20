d677cb4a602acabde43292a4c410c828da5ddb75
Janeucode <enegbedeochuko@gmail.com>
2023-09-20 15:36:23 -0700

    ghjj

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,11 +6,18 @@
  */
 char _strncat(char *dest, char *src, int n)
 {
-	size_t dest_len = strlen(dest);
-	size_t i;
+	int dest_len = 0;
+	int i;
 
-	for (i = 0 ; i < n && src[i] != '\0' ; i++)
-		dest[dest_len + i] = src[i];
-	dest[dest_len + i] = '\0';
+	while (dest[dest_len] != '\0')
+	{
+		dest_len++;
+	}
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[dest_len] = src[i];
+		dest_len++;
+	}
+	dest[dest_len] = '\0';
 	return dest;
 }

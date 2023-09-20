6a0951025dddaf1964d28e6bfc575bea7e22ae85
Janeucode <enegbedeochuko@gmail.com>
2023-09-19 19:48:12 -0700

    sdsd

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main -Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	srand((unsigned int)time(NULL));
+	const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	int charset_size = sizeof(charset) - 1;
+	printf("Generated Password: ");
+	for (int i = 0; i < PASSWORD_LENGTH; i++)
+	{
+		int random_index = rand() % charset_size;
+		char random_char = charset[random_index];
+		_putchar(random_char);
+	}
+	printf("\n");
+	return (0);
+}
+

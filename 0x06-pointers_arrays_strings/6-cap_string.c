a37dc9d0f9f73de0a4183db5fdecc1b086df7058
Janeucode <enegbedeochuko@gmail.com>
2023-09-20 18:16:27 -0700

    sdsds

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdbool.h>
+/**
+ * is_separator - Check if a character is a word separator
+ * @c: The character to check
+ *
+ * Return: true if c is a separator, false otherwise
+ */
+bool is_separator(char c)
+{
+	char separators[] = " \t\n,;.!?\"(){}";
+	int i;
+
+	for (i = 0; separators[i] != '\0'; i++)
+	{
+		if (c == separators[i])
+		{
+			return (true);
+		}
+	}
+	return (false);
+}
+
+/**
+ * cap_string - Capitalize the first letter of each word in a string
+ * @str: Pointer to the input string
+ *
+ * Return: Pointer to the modified string (str)
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (is_separator(str[i]))
+		{
+			i++;
+			continue;
+		}
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - ('a' - 'A');
+		}
+		i++;
+	}
+	return (str);
+}

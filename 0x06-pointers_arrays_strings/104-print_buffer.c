13ee3d4342b4d8d2f49694d4d4fdc12bc0045f2c
Janeucode <enegbedeochuko@gmail.com>
2023-09-21 17:47:35 -0700

    oow

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -15,16 +15,6 @@ char hex_digit(int num)
 		return ('?');
 }
 /**
- * _putchar - Write a character to the standard output (stdout).
- * @c: The character to be written.
- *
- * Return: On success, 1 is returned. On error, -1 is returned.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-/**
  * print_buffer - Print the content of a buffer
  * @b: The buffer to be printed
  * @size: The number of bytes to print

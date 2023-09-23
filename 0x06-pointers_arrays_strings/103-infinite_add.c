c82f1028d4ddb8aea653c29d3fb2aeb0cb3f31a4
Janeucode <enegbedeochuko@gmail.com>
2023-09-22 17:52:58 -0700

    jhsjj

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -14,7 +14,9 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	int len1 = strlen(n1);
 	int len2 = strlen(n2);
 	int carry = 0;
-	int i = len1 - 1, j = len2 - 1, k = 0;
+	int i = len1 - 1;
+	int j = len2 - 1;
+	int k = 0;
 
 	if (size_r <= len1 || size_r <= len2 + 1)
 		return (0);

e5882d507d574575a44942dc2703b7c38bb5aa09
Janeucode <enegbedeochuko@gmail.com>
2023-09-24 17:40:45 -0700

    dios

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,6 +2,3 @@ int _putchar(char c);
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 int -isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_numbers(void);

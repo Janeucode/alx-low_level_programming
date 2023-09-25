e4fabd1717c0ab44e843143258b0d3e18de8bc1e
Janeucode <enegbedeochuko@gmail.com>
2023-09-25 15:27:35 -0700

    try

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _memset - Entry point
+ * @s: pointer to memory area
+ * @b: bytes to be inserted
+ * @n: number of bytes to be inserted
+ * Description: A function that fills memory with a constant byte
+ * Return: char
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *start = s;
+
+	while (n > 0)
+	{
+		*s = b;
+		b++;
+		n--;
+	}
+	return (start);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory covers even more pointers, arrays and strings

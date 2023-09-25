e27facd51fdc09e3c0bcce6989636f1f7b7aaae8
Janeucode <enegbedeochuko@gmail.com>
2023-09-25 15:58:54 -0700

    dsp

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _memcpy - Entry point
+ * @n: number of bytes to be copied
+ * @src: the source byte to copy from
+ * @dest: the destination where the bytes are to be copied to
+ * Description: copy memory area
+ * Return: char
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *d = dest;
+
+	while (n > 0)
+	{
+		*src = *dest;
+		dest++;
+		src++;
+		n--;
+	}
+}

68005999dafe686e9745c05b87ea9bafa8181b7e
Janeucode <enegbedeochuko@gmail.com>
2023-09-25 16:35:41 -0700

    for

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strchr - Entry point
+ * @s: the string to be checked
+ * @c: the character we are looking for
+ * Description: locate a character in a string
+ * Return: NULL
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (NULL)
+}

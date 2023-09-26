21247061049f2afee6fa1fc36d23b3b0ca94e435
Janeucode <enegbedeochuko@gmail.com>
2023-09-25 18:05:21 -0700

    gh

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * _strspn - Entry point
+ * @s: the string
+ * @accept: the substring
+ * Description: for prefix
+ * Return: unsigned int
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
+	char *a;
+	int isMatch;
+
+	while (*s != '\0')
+	{
+		*a = accept;
+		isMatch = 0;
+
+		while (*a != '\0')
+		{
+			if (*s == *a)
+			{
+				isMatch = 1;
+				break;
+			}
+			a++;
+		}
+		if (!isMatch)
+			break;
+		count++;
+		s++;
+	}
+	return (count);
+}

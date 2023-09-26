d12b556e618656cdb7c84905b81669f2b626b33a
Janeucode <enegbedeochuko@gmail.com>
2023-09-25 20:04:13 -0700

    wew

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,21 +8,22 @@
  */
 void print_diagsums(int *a, int size)
 {
+	int i;
 	int sum1 = 0;
 	int sum2 = 0;
 
-	for (int i = 0; i < size; i++)
+	for ( i = 0; i < size; i++)
 	{
 		sum1 += a[i * size + i];
 		sum2 += a[i * size + (size - 1 - i)];
 	}
-	for (int i = 0; i <= sum1 / 10; i++)
+	for ( i = 0; i <= sum1 / 10; i++)
 	{
 		_putchar(sum1 % 10 + '0');
 		sum1 /= 10;
 	}
 	_putchar('\n');
-	for (int i = 0; i <= sum2 / 10; i++)
+	for ( i = 0; i <= sum2 / 10; i++)
 	{
 		_putchar(sum2 % 10 + '0');
 		sum2 /= 10;

e2aff380fdaccd19debf86f5b546054df8d0a890
Janeucode <enegbedeochuko@gmail.com>
2023-09-25 18:32:07 -0700

    klo

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#define NULL ((void *)0)
+/**
+ * _strpbrk: Searches for the first occurrence of any character in accept
+ * Description: search for occurence of character in string
+ * @s: string
+ * @accept: second string
+ * Return: char
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	char *a;
+
+	while (*s != '\0')
+	{
+		a = accept;
+		while (a != '\0')
+		{
+			if (*s == a)
+				return (s);
+			a++;
+		}
+		s++;
+	}
+	return (NULL);
+}

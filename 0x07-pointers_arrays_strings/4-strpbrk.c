6ce0aee3a1a0474dd88ccb7b424c1d9396bf5771
Janeucode <enegbedeochuko@gmail.com>
2023-09-25 18:35:23 -0700

    jkl

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -14,9 +14,9 @@ char *_strpbrk(char *s, char *accept)
 	while (*s != '\0')
 	{
 		a = accept;
-		while (a != '\0')
+		while (*a != '\0')
 		{
-			if (*s == a)
+			if (*s == *a)
 				return (s);
 			a++;
 		}

acb1134754eb7bbd12da3dc6d96993e8e90f2fe4
Janeucode <enegbedeochuko@gmail.com>
2023-09-26 17:46:50 -0700

    jkl

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_diagrams - Entry point
+ * print_diagsums - Entry point
  * @a: one
  * @size: two
  * Description: does whatever
@@ -12,18 +12,18 @@ void print_diagsums(int *a, int size)
 	int sum1 = 0;
 	int sum2 = 0;
 
-	for ( i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		sum1 += a[i * size + i];
 		sum2 += a[i * size + (size - 1 - i)];
 	}
-	for ( i = 0; i <= sum1 / 10; i++)
+	for (i = 0; i <= sum1 / 10; i++)
 	{
 		_putchar(sum1 % 10 + '0');
 		sum1 /= 10;
 	}
 	_putchar('\n');
-	for ( i = 0; i <= sum2 / 10; i++)
+	for (i = 0; i <= sum2 / 10; i++)
 	{
 		_putchar(sum2 % 10 + '0');
 		sum2 /= 10;

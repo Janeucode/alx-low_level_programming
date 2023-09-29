c2c136054a8d9ef1454f34c27414096d9c29948f
Janeucode <enegbedeochuko@gmail.com>
2023-09-28 18:38:04 -0700

    sd

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,12 +7,8 @@
  */
 int _strlen_recursion(char *s)
 {
-	int len = 0;
-
-	if (*s != '\0')
-	{
-		_strlen_recursion(len++);
-		_strlen_recursion(s + 1);
-	}
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
 	return (len);
 }

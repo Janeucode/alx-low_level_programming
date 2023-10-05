a42afc0fa35a12f390709536f8a9eba4a4258405
Janeucode <enegbedeochuko@gmail.com>
2023-10-04 22:00:39 -0700

    sdsds

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,2 +1,30 @@
 #include "main.h"
+int main(int argc, int argv[])
+{
+	int count = 0;
+	int temp;
+	int divisor;
+
+	for (int i = 1; i < argc; i++)
+		count++;
+	if (count >= 0)
+	{
+		temp = count;
+		divisor = 1;
+
+		while (temp >= 10)
+		{
+			temp /= 10;
+			divisor *= 10;
+		}
+		while (divisor > 0)
+		{
+			_putchar('0' + count / divisor);
+			count %= divisor;
+			divisor /= 10;
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
 

00fee12b44b7004f71917a334ba12a16940f27c3
Janeucode <enegbedeochuko@gmail.com>
2023-10-05 18:29:05 -0700

    fdfda

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#define NULL ((void *)0)
+#include <stdlib.h>
+/**
+ * _strdup - Entry point
+ * @str: string to duplicate
+ * Description: pointer to a new string
+ * Return: char
+ */
+char *_strdup(char *str)
+{
+	char *duplicate;
+	int i;
+	int length = 0
+
+	if (str == NULL)
+		return (NULL);
+	while (str[length] != '\0')
+		length++;
+	duplicate = malloc((length + 1) * sizeof(char));
+	if (duplicate == NULL)
+		return (NULL);
+	for (i = 0; i <= length; i++)
+		duplicate[i] = str[i];
+	return (duplicate);
+}

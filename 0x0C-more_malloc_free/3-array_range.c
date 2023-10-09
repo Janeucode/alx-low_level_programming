88e832303708571a941e3490817212e71e5b486f
Janeucode <enegbedeochuko@gmail.com>
2023-10-08 18:51:18 -0700

    dsow

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,26 @@
+#define NULL ((void *)0)
+#include "main.h"
+#include <stdlib.h>
+/**
+ * array_range - Entry point
+ * @min: small
+ * @max: big
+ * Description: create array
+ * Return: int
+ */
+int *array_range(int min, int max)
+{
+	size_t num_elements;
+	int *arr;
+	size_t i;
+
+	if (min > max)
+		return (NULL);
+	num_elements = max - min + 1;
+	*arr = malloc(num_elements * sizeof(int));
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < num_elements; ++i)
+		arr[i] = min + i;
+	return (arr);
+}

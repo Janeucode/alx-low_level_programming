5b3591694011dbecfe163e2528a75ad0df9a77d5
Janeucode <enegbedeochuko@gmail.com>
2023-10-08 17:17:01 -0700

    dspw

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#define NULL ((void *)0)
+/**
+ * malloc_checked - Entry point
+ * @b: the assigned memory
+ * Description: make sense
+ * Return: void
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+this directory still covers mallocs

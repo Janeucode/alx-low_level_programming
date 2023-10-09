3c71b5f94b4b83a341083868dbaa3af4e3c6e2b5
Janeucode <enegbedeochuko@gmail.com>
2023-10-08 17:59:48 -0700

    sdso

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,17 +12,21 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	char *result;
+	size_t len_s1;
+	size_t len_s2;
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	size_t len_s1 = strlen(s1);
-	size_t len_s2 = strlen(s2);
+	len_s1 = strlen(s1);
+	len_s2 = strlen(s2);
 
 	if (n >= len_s2)
 		n = len_s2;
-	char *result = malloc(len_s1 + n + 1);
+	result = malloc(len_s1 + n + 1);
 
 	if (result == NULL)
 		return (NULL);

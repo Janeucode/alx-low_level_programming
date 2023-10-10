ceefbbe732a769d7c0ab6cf0ec633d4b188e28fc
Janeucode <enegbedeochuko@gmail.com>
2023-10-10 16:41:50 -0700

    wongo

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "dog.h"
+#define NULL ((void *)0)
+/**
+ * init_dog - Entry point
+ * @d: struct
+ * @name: the name
+ * @age: the age
+ * @owner: owners name
+ * Description: Initialization
+ * Return:
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d = malloc(sizeof(struct dog));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}

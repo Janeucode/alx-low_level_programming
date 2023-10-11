a5fbe268d6c9f8f97067c6319ddf9f7b83861b21
Janeucode <enegbedeochuko@gmail.com>
2023-10-10 19:41:25 -0700

    sds

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#define NULL ((void *)0)
+#include <stdlib.h>
+/**
+ * free_dog - Entry point
+ * @d: dog
+ * Description: play
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}

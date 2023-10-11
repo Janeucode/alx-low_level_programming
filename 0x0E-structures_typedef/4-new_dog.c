6ae3991421a986c87cc077aec0ed786821a4a853
Janeucode <enegbedeochuko@gmail.com>
2023-10-10 19:26:11 -0700

    dfsjko

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,33 @@
+#include "dog.h"
+#define NULL ((void *)0)
+#include <stdlib.h>
+/**
+ * new_dog: Entry point
+ * @name: name
+ * @age: age
+ * @owner: na ham
+ * Description: new dog
+ * Return:
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog = malloc(sizeof(dog_t));
+
+	if (new_dog == NULL)
+		return (NULL);
+	new_dog->name = strdup(name);
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	new_dog->age = age;
+	new_dog->owner = strdup(owner);
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
+	return new_dog;
+}

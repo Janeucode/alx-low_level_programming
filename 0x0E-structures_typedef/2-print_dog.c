96fb3988a9154cdc55d5d5449d89a36c4dae175a
Janeucode <enegbedeochuko@gmail.com>
2023-10-10 18:45:14 -0700

    sdow

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,7 +13,7 @@ void print_dog(struct dog *d)
 		return;
 	printf("Name: %s\n", d->name != NULL ? d->name : "nil\n");
 	if (d->age >= 0)
-		printf("Age: %f\n", d->age );
+		printf("Age: %f\n", d->age);
 	else
 		printf("Age: nil\n");
 	printf("Owner: %s\n", d->owner != NULL ? d->owner : "nil\n");

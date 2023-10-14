da33dad1ee4ad5f33b9f5cdcf447155157e81ed9
Janeucode <enegbedeochuko@gmail.com>
2023-10-13 18:22:48 -0700

    trial

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,7 +1,9 @@
 #include "variadic_functions.h"
-#define VA_START(ap, last_arg)   ((ap) = (va_list)&(last_arg) + sizeof(last_arg))
-#define VA_ARG(ap, type)         (*(type*)((ap) += sizeof(type), (ap) - sizeof(type)))
-#define VA_END(ap)               ((void)(ap))
+#define VA_START(ap, last_arg) ((ap) = (va_list)&(last_arg) + sizeof(last_arg))
+#define VA_ARG(ap, type) (*(type*)((ap) += sizeof(type), (ap) - sizeof(type)))
+#define VA_END(ap) ((void)(ap))
+
+typedef char* va_list;
 /**
  * sum_them_all - Entry point
  * @n: number
@@ -14,12 +16,12 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	va_list args;
 
-	va_start(args, n);
+	VA_START(args, n);
 
 	if (n == 0)
 		return (0);
 	for (i = 0; i < n; i++)
-		sum += va_arg(args, int);
-	va_end(args);
+		sum += VA_ARG(args, int);
+	VA_END(args);
 	return (sum);
 }

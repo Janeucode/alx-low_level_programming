7b6e972503ecf2b07ef4caf746953e37323eed37
Janeucode <enegbedeochuko@gmail.com>
2023-10-13 19:05:48 -0700

    opw

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ * print_numbers - Entry point
+ * @seperator: seperate
+ * @n: number
+ * Description: to doo
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i = 0
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+	}
+	va_end(args);
+	printf("\n");
+}

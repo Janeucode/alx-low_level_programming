41fb98db7117ca22744eb15fe4dd7297e2f9e81a
Janeucode <enegbedeochuko@gmail.com>
2023-10-13 19:45:50 -0700

    joko

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -14,36 +14,36 @@ void print_all(const char * const format, ...)
 	char *str;
 	int i = 0;
 	int print_separator = 0;
-	
+
 	va_start(args, format);
 
-	 while (format && format[i])
-	 {
-		 if (format[i] == 'c')
-		 {
-			 printf("%c", va_arg(args, int));
-			 print_separator = 1;
-		 }
-		 else if (format[i] == 'i')
-		 {
-			 printf("%f", (float)va_arg(args, double));
-			 print_separator = 1;
-		 }
-		 else if (format[i] == 's')
-		 {
-			 str = va_arg(args, char *);
-			 if (str == NULL)
-				 str = "(nil)";
-			 printf("%s", str);
-			 print_separator = 1;
-		 }
-		 if (print_separator && format[i + 1] != '\0')
-		 {
-			 printf(", ");
-			 print_separator = 0;
-		 }
-		 i++;
-	 }
-	 va_end(args);
-	 printf("\n");
+	while (format && format[i])
+	{
+		if (format[i] == 'c')
+		{
+			printf("%c", va_arg(args, int));
+			print_separator = 1;
+		}
+		else if (format[i] == 'i')
+		{
+			printf("%f", (float)va_arg(args, double));
+			print_separator = 1;
+		}
+		else if (format[i] == 's')
+		{
+			str = va_arg(args, char *);
+			if (str == NULL)
+				str = "(nil)";
+			printf("%s", str);
+			print_separator = 1;
+		}
+		if (print_separator && format[i + 1] != '\0')
+		{
+			printf(", ");
+			print_separator = 0;
+		}
+		i++;
+	}
+	va_end(args);
+	printf("\n");
 }

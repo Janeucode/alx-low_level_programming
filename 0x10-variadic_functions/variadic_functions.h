06d50d4bc661933efd1c38466da4ca3256639073
Janeucode <enegbedeochuko@gmail.com>
2023-10-13 18:03:28 -0700

    fkdop

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,7 @@
+#ifndef MY_HEADER_H
+#define MY_HEADER_H
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+#endif

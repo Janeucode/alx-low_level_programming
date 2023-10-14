e244a765300159efe2a1a81a623d1ccbe2962171
Janeucode <enegbedeochuko@gmail.com>
2023-10-13 18:55:44 -0700

    sdl

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,9 +1,5 @@
 #include "variadic_functions.h"
-#define VA_START(args, last_arg)   ((args) = (va_list)&(last_arg) + sizeof(last_arg))
-#define VA_ARG(args, type)         (*(type*)((args) += sizeof(type), (args) - sizeof(type)))
-#define VA_END(args)               ((void)(args))
-
-typedef char* va_list;
+#include <stdarg.h>
 /**
  * sum_them_all - Entry point
  * @n: number
@@ -16,12 +12,12 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	va_list args;
 
-	VA_START(args, n);
+	va_start(args, n);
 
 	if (n == 0)
 		return (0);
 	for (i = 0; i < n; i++)
-		sum += VA_ARG(args, int);
-	VA_END(args);
+		sum += va_arg(args, int);
+	va_end(args);
 	return (sum);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,5 @@
 #ifndef MY_HEADER_H
 #define MY_HEADER_H
-#define VA_START(ap, last_arg) ((ap) = (va_list)&(last_arg) + sizeof(last_arg))
-#define VA_ARG(ap, type) (*(type*)((ap) += sizeof(type), (ap) - sizeof(type)))
-#define VA_END(ap) ((void)(ap))
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);

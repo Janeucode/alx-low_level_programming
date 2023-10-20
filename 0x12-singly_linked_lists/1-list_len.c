d2dbb7eea9a854c3492304b1a2a1c7fc6c0863f4
Janeucode <enegbedeochuko@gmail.com>
2023-10-19 19:30:07 -0700

    list lenght

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * list_len - Entry point
+ * @h: content
+ * Description: print elements
+ * Return: sixe_t
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}

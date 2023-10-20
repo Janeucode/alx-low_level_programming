32fe5d2593d0139e4674ca07734d2c2b4a09c9d3
Janeucode <enegbedeochuko@gmail.com>
2023-10-19 19:14:55 -0700

    lopo

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 /**
  * print_list - Entry point
@@ -9,16 +8,16 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	size_t node_count = 0;
 
 	while (h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("%s\n", h->str);
-		count++;
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
+		node_count++;
 	}
-	return (count);
+	return (node_count);
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,6 @@
 typedef struct list_s{
-        char *str;
+	char *str;
+        unsigned int len;
         struct list_s *next;
 } list_t;
 int _putchar(char c);

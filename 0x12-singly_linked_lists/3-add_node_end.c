cf384134a596ad54ce772f5e707f09e7c573c818
Janeucode <enegbedeochuko@gmail.com>
2023-10-20 17:10:39 -0700

    2nd try to add node

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,10 +11,13 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
+	list_t *new_node;
+	list_t *current;
+
 	if (str == NULL)
 		return (NULL);
 
-	list_t *new_node = malloc(sizeof(list_t));
+	new_node = malloc(sizeof(list_t));
 
 	if (new_node == NULL)
 		return (NULL);
@@ -31,10 +34,10 @@ list_t *add_node_end(list_t **head, const char *str)
 	if (*head == NULL)
 		*head = new_node;
 	else
-		list_t *current = *head;
+		current = *head;
 	while (current->next != NULL)
 		current = current->next;
 	current->next = new_node;
-	}
+
 	return (new_node);
 }

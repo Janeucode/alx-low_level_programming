24071a072fc4742335a31db3e5d0313456e300de
Janeucode <enegbedeochuko@gmail.com>
2023-10-24 18:30:24 -0700

    print linked list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_listint - Entry point
+ * @h: input
+ * Description: number of nodes
+ * Return: size_t
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This directory covers more linked list

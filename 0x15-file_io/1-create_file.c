179d6c3cc6f7440b2815b8dbffbf6989cbc8e5ed
Janeucode <enegbedeochuko@gmail.com>
2023-10-30 19:22:14 -0700

    correct permissions

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	file_descriptor = open(filename, O_CREAT, O_WRONLY, O_TRUNC, 0600);
+	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (file_descriptor == -1)
 		return (-1);

9f17ecabc031abbdaf8f13ccb4c5d73918f665a7
Janeucode <enegbedeochuko@gmail.com>
2023-12-07 16:37:02 -0800

    get number of elements

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+/**
+ *  dlistint_len - Entry point
+ *  @h: pointer to the header
+ *  Descriptino: gets lent of doubly linked list
+ *  Return: i
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i = 0;
+	const dlistint_t *current = h;
+
+	while (current != NULL)
+	{
+		i++;
+		current = current->next;
+	}
+	return (i);		
+}
